6978ec903baab4fb7ca9d50e37b3a3556aad19eb
BackStreetSoy <jordanedwardobey@gmail.com>
2018-11-21 18:01:43 -0500

    cleaned the layout

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-var bodyParser = require("body-parser"),
-express        = require("express"),
-methodOverride = require("method-override"),
-mongoose       = require("mongoose"),
-app            = express();
+var bodyParser   = require("body-parser"),
+methodOverride   = require("method-override"),
+expressSanitizer = require("express-sanitizer"),
+express          = require("express"),
+mongoose         = require("mongoose"),
+app              = express();
 
 
 mongoose.connect('mongodb://localhost:27017/blog_app', { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride('_method'));
 
 
@@ -53,7 +55,7 @@ app.get("/blogs/new", function(req, res){
 app.post("/blogs", function(req, res){
     //create blog
     //redirect
-
+    req.body.blog.body = req.sanitize(req.body.blog.body)
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render("new");
@@ -93,6 +95,7 @@ app.get("/blogs/:id/edit", function(req, res){
 //UPDATE ROUTE
 
 app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body)
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err,updatedBlog){
         if(err){
             res.redirect("/blogs");
